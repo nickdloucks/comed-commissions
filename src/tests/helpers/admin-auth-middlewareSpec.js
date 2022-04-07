7cea9fc5e4f9fd93022a26e6a624a49cf69fffb1
Nick <nickdloucks@outlook.com>
2022-04-06 20:19:36 -0700

    outline specs for admin dash, auth middleware

---
+++ src/tests/helpers/admin-auth-middlewareSpec.js
@@ -0,0 +1,15 @@
+import jasmine from "jasmine";
+
+describe("The middleware for authenticating an admin user.", ()=> {
+    it("Verifies the user correectly.", ()=>{
+
+    });
+    
+    it("Verifies the password correctly", ()=>{
+
+    });
+
+    it("Does not allow access with password logic flaws.", ()=>{
+
+    });
+});
---
+++ src/tests/helpers/admin-dashSpec.js
@@ -0,0 +1,15 @@
+import jasmine from "jasmine";
+
+describe("The administrator dashboard for configuring commissions parameters.", ()=> {
+    it("Displays the correct list of sales reps", ()=>{
+
+    });
+    
+    it("Changes OTD commission percentage correctly", ()=>{
+
+    });
+
+    it("Changes specific DIR manufacturer commission percentage correctly", ()=>{
+
+    });
+});
