f77d7abb3e8de0a6ad86d1fd06855a295db17e04
Nick <nickdloucks@outlook.com>
2022-04-07 09:34:37 -0700

    start admin-dash endpoint

--- src/routes/admin-dash.js
+++ src/routes/admin-dash.js
@@ -2,8 +2,9 @@ import { express } from "express";
 
 let app = express();
 
-// app.get('/admin', req, res){
+app.get('/views/admin-dash.html', (req, res)=>{
+    res.send('/views/admin-login.html');
+});
     // middleware with authentication
     // this is where admin will set commissions parameters
     // admin will need a username and password, maybe even 2FA if I can figure out how to implement it
-// }
