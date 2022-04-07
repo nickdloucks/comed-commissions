99f039d3a5060bc892e04c24a94b5bff351fc213
Nick <nickdloucks@outlook.com>
2022-04-06 20:18:11 -0700

    start outlining admin route/enpoint

---
+++ src/routes/admin-dash.js
@@ -0,0 +1,9 @@
+import { express } from "express";
+
+let app = express();
+
+// app.get('/admin', req, res){
+    // middleware with authentication
+    // this is where admin will set commissions parameters
+    // admin will need a username and password, maybe even 2FA if I can figure out how to implement it
+// }
