b6b29d08677e7ab3416924284277b4817d371237
Nick <nickdloucks@outlook.com>
2022-04-11 15:53:32 -0700

    refactor idea: URL params to call auth middleware

--- src/routes/admin-dash.js
+++ src/routes/admin-dash.js
@@ -9,17 +9,24 @@ const PASSWORDS = [ process.env.ADMIN_USER1_PASSWORD ];
 let app = express();
 
 adminRouter.get('/views/admin-dash.html', (req, res)=>{
-    let credentials = req.params;
-    if(USERS.includes(credentials.user) && (PASSWORDS[USERS.indexOf(credentials.user)] === credentials.password)){
-        res.send('/views/admin-dash.html');
-    }else{
-        res.send('/views/admin-login.html');
-    }
+    
+    res.redirect('./views/admin-login.html');
+    
     // Be sure not to cause an infinite loop of redirection... 
     // need to find a way to block unauthorized access to the url besides just omitting a link in the homepage?
 });
     // middleware with authentication
     // this is where admin will set commissions parameters
     // admin will need a username and password, maybe even 2FA if I can figure out how to implement it
+
+adminRouter.get('/views/admin-dash.html?authenticate=true', (req, res)=>{ // link this URL to login Submit button on admin-login page
+    
+    // let credentials = req.params;
+    // if(USERS.includes(credentials.user) && (PASSWORDS[USERS.indexOf(credentials.user)] === credentials.password)){
+    //     res.send('/views/admin-dash.html');
+    // }else{
+    //     res.send('/views/admin-login.html');
+    // }
+});
 
 export default adminRouter;
