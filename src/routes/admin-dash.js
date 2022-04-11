134ac7713de61dbd5fb0511c2d60e4030cf46652
Nick <nickdloucks@outlook.com>
2022-04-11 13:30:37 -0700

    set up admin routes to be modular

--- src/routes/admin-dash.js
+++ src/routes/admin-dash.js
@@ -1,21 +1,25 @@
 import 'dotenv/config';
 import express from "express";
 
+const adminRouter = express.Router();
+
 const USERS = [ process.env.ADMIN_USER1 ];
 const PASSWORDS = [ process.env.ADMIN_USER1_PASSWORD ];
 
 let app = express();
 
-app.get('/views/admin-dash.html', (req, res)=>{
+adminRouter.get('/views/admin-dash.html', (req, res)=>{
     let credentials = req.params;
     if(USERS.includes(credentials.user) && (PASSWORDS[USERS.indexOf(credentials.user)] === credentials.password)){
         res.send('/views/admin-dash.html');
     }else{
         res.send('/views/admin-login.html');
     }
-     // Be sure not to cause an infinite loop of redirection... 
+    // Be sure not to cause an infinite loop of redirection... 
     // need to find a way to block unauthorized access to the url besides just omitting a link in the homepage?
 });
     // middleware with authentication
     // this is where admin will set commissions parameters
     // admin will need a username and password, maybe even 2FA if I can figure out how to implement it
+
+export default adminRouter;
--- src/tests/helpers/admin-auth-middlewareSpec.js
+++ src/tests/helpers/admin-auth-middlewareSpec.js
@@ -1,4 +1,5 @@
 import jasmine from "jasmine";
+import { default as adminRouter } from 'admin-dash.js';
 
 describe("The middleware for authenticating an admin user.", ()=> {
     it("Verifies the user correectly.", ()=>{
