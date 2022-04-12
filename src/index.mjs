1318e81aba53c4be327727620759f80bdde18440
Nick <nickdloucks@outlook.com>
2022-04-12 09:33:38 -0700

    save current rep's config data in variable

--- src/index.mjs
+++ src/index.mjs
@@ -1,20 +1,21 @@
 // import fsPromises from 'fs';
+
+let currentRepParams = {}; // variable for the commissions parameters of the currently selected rep
+
 const repSelect = document.getElementById('rep-select');
-
-
 repSelect.addEventListener('input',()=>{
     let repName = repSelect.value;
     let configFile = new String(repName).concat('.json');
     let configFilePath = '../fs_db/commission_configs/'.concat(configFile);
-    console.log(configFilePath);
+    // console.log(configFilePath);
 
     fetch(configFilePath)
         .then(response =>{
             return response.json();
         })
         .then(data =>{
-            console.log(data.rep_name); // LOGS DATA FROM JSON FILE!!!
+            currentRepParams = data;
+            console.log(currentRepParams.rep_name);
         });
-
 })
 
