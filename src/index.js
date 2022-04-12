8239d382b2b50ffe86b26d4452e037ada9ca15c7
Nick <nickdloucks@outlook.com>
2022-04-11 19:52:05 -0700

    reads json data based on rep-select

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+// import fsPromises from 'fs';
 const repSelect = document.getElementById('rep-select');
 
 
 repSelect.addEventListener('input',()=>{
     let repName = repSelect.value;
-    let configFilePath = new String(repName).concat('.json');
+    let configFile = new String(repName).concat('.json');
+    let configFilePath = '../fs_db/commission_configs/'.concat(configFile);
     console.log(configFilePath);
+
+    fetch(configFilePath)
+        .then(response =>{
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data); // LOGS DATA FROM JSON FILE!!!
+        });
+
 })
 
